bc5ba4c615b61b622aa8a740e6a87eddd87321f1
JORGEVILLARREAL <>
2022-04-21 23:46:36 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -128,7 +128,7 @@ Pair * searchTreeMap(TreeMap * tree, void* key)
     else
     {
       if(tree->lower_than(aux->pair->key, key) == 1)
-        aux = aux;
+        aux = aux->right;
       else
       {
         tree->current = aux;
